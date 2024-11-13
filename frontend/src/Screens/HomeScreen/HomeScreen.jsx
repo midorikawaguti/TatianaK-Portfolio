f559f785f58960ed3e577c626872d879025c55f7
Tatiana Kawaguti <midori.kawaguti@yahoo.com.br>
2024-11-12 22:18:24 -0500

    Updated HomeScreen

--- frontend/src/Screens/HomeScreen/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen/HomeScreen.jsx
@@ -5,6 +5,9 @@ import './HomeScreen.css';
 import Project from '../../Components/Project/Project.jsx';
 import About from '../../Components/About/About.jsx'
 import projects from '../../projects';
+import { ReactTyped as Typed } from 'react-typed';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'
+
 
 const HomeScreen = () => {
   const scrollToSection = (sectionId) => {
@@ -27,13 +30,24 @@ const HomeScreen = () => {
           </Col>
           <Col md={12} lg={5} xl={6} className="allText">
             <h1 className="text1">Hi, I'm Tatiana M. Kawaguti</h1>
-            <h3 className="text2" id="typed-text">I am an aspiring Software Engineer</h3>
+            <h3 className="text2">
+            <Typed
+              strings={[
+                'A Full Stack Developer',
+                'A creative coder',
+                'A problem solver',
+              ]}
+              typeSpeed={180}
+              backSpeed={70}
+              loop
+            />
+            </h3>
             <p className="text3">
               I was born and raised in Brazil, but I recently moved to Canada to embark on a new adventure.
               I believe in the positive impact that technological tools can have, which motivates me to
               continue learning and growing in this field.
             </p>
-            <div className="buttons">
+            <div className="homeScreenButtons">
               <Button onClick={() => scrollToSection('projects')}>Projects</Button>
               <Button onClick={downloadCV}>Download CV</Button>
             </div>
@@ -41,23 +55,19 @@ const HomeScreen = () => {
         </Row>
 
         <Row className="container-2">
-          <ul className="list-icons">
-            <li>
+          <Col sm={12} md={6} lg={4} xl={4} >
               <a href="https://github.com/midorikawaguti" target="_blank" rel="noopener noreferrer">
-                <i className="bi bi-github fa-2x text-secondary m-3"></i>
+                <FaGithub/>
               </a>
-            </li>
-            <li>
+
               <a href="https://www.linkedin.com/in/tatiana-midori-uemura-kawaguti-6865b5120/" target="_blank" rel="noopener noreferrer">
-                <i className="bi bi-linkedin fa-2x text-secondary m-3"></i>
+                <FaLinkedin/>
               </a>
-            </li>
-            <li>
+
               <a href="mailto:midori.kawaguti@gmail.com">
-                <i className="bi bi-envelope-fill fa-2x text-secondary m-3"></i>
+                <FaEnvelope/>
               </a>
-            </li>
-          </ul>
+          </Col>
         </Row>
 
         <div id="about-me">
